5acd43cd8db1d809df75c7e4949f5e4c555a8acc
zakzakst <e1433104@gmail.com>
2025-12-22 14:31:55 +0900

    checkbox

---
+++ src/components/checkbox/variants.ts
@@ -0,0 +1,76 @@
+import { cva } from 'class-variance-authority'
+
+export const checkboxVariants = cva(
+  [
+    'flex items-center justify-center shrink-0 rounded-[calc(1/8*100%)]',
+    'has-[input:hover:not(:focus):not([aria-disabled="true"])]:bg-solid-gray-420',
+  ],
+  {
+    variants: {
+      size: {
+        sm: 'size-6',
+        md: 'size-8',
+        lg: 'size-11',
+      },
+    },
+    defaultVariants: {
+      size: 'sm',
+    },
+  }
+)
+
+export const checkboxInputVariants = cva(
+  [
+    'appearance-none size-3/4 rounded-[calc(2/18*100%)] border-solid-gray-600 bg-white bg-clip-padding',
+    'hover:border-black',
+    'focus:outline focus:outline-4 focus:outline-black focus:outline-offset-[calc(2/16*1rem)] focus:ring-[calc(2/16*1rem)] focus:ring-yellow-300',
+    'checked:border-blue-900 checked:bg-blue-900 checked:hover:border-blue-1100 checked:hover:bg-blue-1100',
+    'indeterminate:border-blue-900 indeterminate:bg-blue-900 indeterminate:hover:border-blue-1100 indeterminate:hover:bg-blue-1100',
+    'before:hidden before:size-3.5 before:bg-white',
+    "checked:before:block checked:before:[clip-path:path('M5.6,11.2L12.65,4.15L11.25,2.75L5.6,8.4L2.75,5.55L1.35,6.95L5.6,11.2Z')]",
+    "indeterminate:before:block indeterminate:before:[clip-path:path('M3.25,7.75H10.75V6.25H3.25V7.75Z')]",
+    'aria-disabled:!border-solid-gray-300 aria-disabled:!bg-solid-gray-50 aria-disabled:checked:!bg-solid-gray-300 aria-disabled:indeterminate:!bg-solid-gray-300 aria-disabled:before:border-solid-gray-50',
+    'forced-colors:!border-[ButtonText] forced-colors:checked:!bg-[Highlight] forced-colors:checked:!border-[Highlight] forced-colors:indeterminate:!bg-[Highlight] forced-colors:indeterminate:!border-[Highlight] forced-colors:before:!bg-[HighlightText] forced-colors:aria-disabled:!border-[GrayText] forced-colors:aria-disabled:checked:!bg-[GrayText]',
+  ],
+  {
+    variants: {
+      size: {
+        sm: 'border-[calc(2/16*1rem)]',
+        md: 'border-[calc(2/16*1rem)] before:origin-top-left before:scale-[calc(20/14)]',
+        lg: 'border-[calc(3/16*1rem)] before:origin-top-left before:scale-[calc(27/14)]',
+      },
+      isError: {
+        true: 'border-error-1 hover:border-red-1000 checked:bg-error-1 checked:hover:bg-red-1000 indeterminate:bg-error-1 indeterminate:hover:bg-red-1000',
+      },
+    },
+    defaultVariants: {
+      size: 'sm',
+    },
+  }
+)
+
+export const checkboxLabelVariants = cva('flex w-fit items-start py-2', {
+  variants: {
+    size: {
+      sm: 'gap-1',
+      md: 'gap-2',
+      lg: 'gap-2',
+    },
+  },
+  defaultVariants: {
+    size: 'sm',
+  },
+})
+
+export const checkboxLabelTextVariants = cva('text-solid-gray-800', {
+  variants: {
+    size: {
+      sm: 'pt-px text-dns-16N-130',
+      md: 'pt-1 text-dns-16N-130',
+      lg: 'pt-2.5 text-dns-17N-130',
+    },
+  },
+  defaultVariants: {
+    size: 'sm',
+  },
+})
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const inputValue = ref(6)
+const checkedValue = ref(false)
 </script>
 
 <template>
@@ -8,4 +9,7 @@ const inputValue = ref(6)
   <Divider color="gray-420" class="border-t-2" />
   <Input type="number" name="test" v-model="inputValue" step="0.1" />
   <p>{{ inputValue }}</p>
+  <!-- <Checkbox v-model="checkedValue">Check Label</Checkbox> -->
+  <Checkbox v-model="checkedValue" />
+  <p>{{ JSON.stringify(checkedValue) }}</p>
 </template>
